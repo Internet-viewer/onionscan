bc3f55e0ff18dce058eb6b6e14e95735f1c201db
Internet-viewer <74160354+Internet-viewer@users.noreply.github.com>
2021-09-22 13:42:47 +0800

    Update protocol_scanner.go

--- protocol/protocol_scanner.go
+++ protocol/protocol_scanner.go
@@ -1,8 +1,8 @@
 package protocol
 
 import (
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/report"
+	"github.com/Internet-viewer/onionscan/config"
+	"github.com/Internet-viewer/onionscan/report"
 )
 
 type Scanner interface {
