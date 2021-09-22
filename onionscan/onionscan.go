d143d7a03d59d38fe87e677ec433afced62b1d27
Internet-viewer <74160354+Internet-viewer@users.noreply.github.com>
2021-09-22 13:40:25 +0800

    Update onionscan.go

--- onionscan/onionscan.go
+++ onionscan/onionscan.go
@@ -2,9 +2,9 @@ package onionscan
 
 import (
 	"fmt"
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/protocol"
-	"github.com/s-rah/onionscan/report"
+	"github.com/Internet-viewer/onionscan/config"
+	"github.com/Internet-viewer/onionscan/protocol"
+	"github.com/Internet-viewer/onionscan/report"
 	"time"
 )
 
