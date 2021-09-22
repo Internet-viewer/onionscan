71033531950fd2db0983646f01d1106c4888c245
Internet-viewer <74160354+Internet-viewer@users.noreply.github.com>
2021-09-22 13:46:16 +0800

    Update onionspider.go

--- spider/onionspider.go
+++ spider/onionspider.go
@@ -3,9 +3,9 @@ package spider
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/model"
-	"github.com/s-rah/onionscan/report"
+	"github.com/Internet-viewer/onionscan/config"
+	"github.com/Internet-viewer/onionscan/model"
+	"github.com/Internet-viewer/onionscan/report"
 	"golang.org/x/net/proxy"
 	"net/http"
 	"net/http/cookiejar"
