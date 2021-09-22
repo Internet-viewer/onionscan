35d54c338c461d36d569eea55b7e4cf509198b8d
Internet-viewer <74160354+Internet-viewer@users.noreply.github.com>
2021-09-22 13:27:01 +0800

    Update validation.go

--- utils/validation.go
+++ utils/validation.go
@@ -3,10 +3,12 @@ package utils
 import (
 	"regexp"
 	"strings"
+	"fmt"
 )
 
 func IsOnion(identifier string) bool {
 	// TODO: At some point we will want to support i2p
+	fmt.Println(`"56characterssupportadded."`)
 	if len(identifier) >= 62 && strings.HasSuffix(identifier, ".onion") {
 		matched, _ := regexp.MatchString(`(^|\.)[a-z2-7]{16,56}\.onion$`, identifier)
 		return matched
